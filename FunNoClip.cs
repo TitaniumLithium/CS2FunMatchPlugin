d098fd9d01560de34e46ed59c4341e90c52a5e69
TitaniumLithium <48680707+TitaniumLithium@users.noreply.github.com>
2024-06-13 23:59:22 +0800

    Update Noclip Mode
    Noclip Now enable in interval seconds

--- FunNoClip.cs
+++ FunNoClip.cs
@@ -4,35 +4,65 @@ using CounterStrikeSharp.API.Core;
 
 namespace FunMatchPlugin;
 
+using CounterStrikeSharp.API.Modules.Timers;
 public class FunNoClip : FunBaseClass
 {
     public override string Decription => "NoClip ON 启用飞行";
     private List<CCSPlayerController> Allplayers = new();
     public FunNoClip(FunMatchPlugin plugin) : base(plugin){}
+    private Timer ?NoclipOnTimer = null;
+    private bool IsNoClipON = false;
+    public float interval = 2.0f;
+
     public override void Fun(FunMatchPlugin plugin)
     {
         if (Enabled) return;
         Enabled = true;
-        ConVar.Find("sv_cheats")!.SetValue(Enabled);
-        Allplayers = Utilities.GetPlayers();
-        foreach (var p in Allplayers)
-        {
-            //if (p.IsBot) continue;
-            p.ExecuteClientCommandFromServer("noclip 1");
-        }
+        IsNoClipON = false;
+        NoclipOnTimer = plugin.AddTimer(interval,SetNoclip,TimerFlags.REPEAT);
     }
     public override void EndFun(FunMatchPlugin plugin)
     {
         Allplayers = Utilities.GetPlayers();
         foreach (var p in Allplayers)
         {
-            if (p.IsValid && !p.IsBot)
+            if (p.IsValid)
             {
                 p.ExecuteClientCommandFromServer("noclip 0");              
             }
         }
         Enabled = false;
-        ConVar.Find("sv_cheats")!.SetValue(Enabled);
+        IsNoClipON = false;
+        ConVar.Find("sv_cheats")!.SetValue(false);
         Allplayers.Clear();
+        if (NoclipOnTimer is not null) NoclipOnTimer.Kill();
+    }
+
+    private void SetNoclip()
+    {
+        if (IsNoClipON is false)
+        {
+            ConVar.Find("sv_cheats")!.SetValue(true);
+            Allplayers = Utilities.GetPlayers();
+            foreach (var p in Allplayers)
+            {
+                //if (p.IsBot) continue;
+                p.ExecuteClientCommandFromServer("noclip 1");
+            }
+            IsNoClipON = true;
+            ConVar.Find("sv_cheats")!.SetValue(false);
+        }
+        else
+        {
+            ConVar.Find("sv_cheats")!.SetValue(true);
+            Allplayers = Utilities.GetPlayers();
+            foreach (var p in Allplayers)
+            {
+                //if (p.IsBot) continue;
+                p.ExecuteClientCommandFromServer("noclip 0");
+            }
+            IsNoClipON = false;
+            ConVar.Find("sv_cheats")!.SetValue(false);
+        }
     }
 }
