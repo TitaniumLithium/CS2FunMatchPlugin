ac50b129e3a0887266763202b330d4684868ce5f
TitaniumLithium <48680707+TitaniumLithium@users.noreply.github.com>
2024-06-22 23:33:51 +0800

    disable(CS8618warning)
    disable(CS8618warning)

--- CustomModesConfig.cs
+++ CustomModesConfig.cs
@@ -1,5 +1,5 @@
 ﻿namespace FunMatchPlugin;
-
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
 public class CustomModeInfo
 {
     public string Decr {get;set;}
@@ -9,5 +9,8 @@ public class CustomModeInfo
 
 public class CustomModesConfig
 {
+
     public List<CustomModeInfo> Modes{get;set;}
 }
+
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
